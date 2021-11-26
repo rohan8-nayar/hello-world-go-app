98c7f421076c0c7fb19252a2327e13205a709608
root <root@ip-172-31-11-23.ec2.internal>
2021-11-26 06:36:48 +0000

    testing 9

--- main.go
+++ main.go
@@ -12,7 +12,7 @@ type Server struct{}
 func (s *Server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusOK)
 	w.Header().Set("Content-Type", "application/json")
-	w.Write([]byte(`{"message": "Hello World!"}`))
+	w.Write([]byte(`{"message": "hello world"}`))
 }
 
 func main() {
